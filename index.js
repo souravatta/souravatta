52e638947e430cb4abaf94cb7906d014cdc07b4e
sourav <sourav.atta19395@gmail.com>
2021-02-09 19:30:24 +0530

    Changed Company Name in Readme.md

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ const readme = `\
 ![README-CI](https://github.com/souravatta/souravatta/workflows/README-CI/badge.svg?branch=master)  ![Stackoverflow-Stats-CI](https://github.com/souravatta/souravatta/workflows/Stackoverflow-Stats-CI/badge.svg)
 # Hey there 👋, I am Sourav Atta
 
-I am currently working as a DevOps engineer. As a DevOps engineer, I always try to be very tool agnostic. I do am fond of some particular tools like 
+I am currently working as a DevOps engineer. As a DevOps engineer, I always try to be very tool agnostic. I do am fond of some particular tools like
 ELK, Ansible, Sonarqube and Docker, but I sure can work on other tools with utmost efficiency. Apart from that, I am very much into learning old-school IT concepts.
 
 <img align="right" alt="GIF" src="https://media.giphy.com/media/eMPormrWOvXQHSKTjO/giphy.gif"/>
 
 **Know more about me**
-- 🏢 I’m currently working in [Wipro Technologies](https://www.wipro.com/en-IN/about-us/)
-- 🌱 I’m currently learning Cloud Native Technologies eg. Docker, K8's
+- 🏢 I’m currently working in [Talentica Software](https://www.talentica.com/)
+- 🌱 I’m currently learning Cloud Native Technologies eg. K8's, Terraform
 - ⚡️ Fun-Fact: Strategically lazy😎
 - ⏳ Interests: Travelling, Cooking, Video Games
 - 📝[Resume](https://drive.google.com/file/d/19W1KxeMzk6YGuw--yOGLxT3vjxfxIRTk/view?usp=sharing)
@@ -44,7 +44,7 @@ ELK, Ansible, Sonarqube and Docker, but I sure can work on other tools with utmo
 
 
   **Stackoverflow Stats**
-  
+
   <br>
   <img alt="IMG" src="https://github-readme-stackoverflow.vercel.app/?userID=8854824"/>
   <hr>
@@ -64,21 +64,23 @@ ELK, Ansible, Sonarqube and Docker, but I sure can work on other tools with utmo
 |Build Tools                   |![](https://img.shields.io/badge/Maven-3-9400D3?labelColor=7D898B) ![](https://img.shields.io/badge/npm-3-9400D3?labelColor=7D898B)                                                                                  |
 |Code Quality                  |![](https://img.shields.io/badge/Sonarqube-4-orange?labelColor=7D898B) ![](https://img.shields.io/badge/Jacoco-1-blue?labelColor=7D898B)                                                                             |
 |Collaboration                 |![](https://img.shields.io/badge/JIRA-3-9400D3?labelColor=7D898B) ![](https://img.shields.io/badge/Confluence-3-9400D3?labelColor=7D898B)                                                                            |
-|Containerization              |![](https://img.shields.io/badge/Docker-3-9400D3?labelColor=7D898B)                                                                                                                                                  |  
+|Containerization              |![](https://img.shields.io/badge/Docker-3-9400D3?labelColor=7D898B)                                                                                                                                                  |
 |Test Automation               |![](https://img.shields.io/badge/Selenium-2-green?labelColor=7D898B) ![](https://img.shields.io/badge/TOSCA-2-green?labelColor=7D898B)                                                                               |
 |Source Control Tools          |![](https://img.shields.io/badge/GIT-3-9400D3?labelColor=7D898B) ![](https://img.shields.io/badge/Gitlab-3-9400D3?labelColor=7D898B) ![](https://img.shields.io/badge/Github-3-9400D3?labelColor=7D898B)             |
 |Repository Management         |![](https://img.shields.io/badge/Nexus-3-9400D3?labelColor=7D898B)                                                                                                                                                   |
 |Continuous Integration        |![](https://img.shields.io/badge/Jenkins-3-9400D3?labelColor=7D898B) ![](https://img.shields.io/badge/Gitlab%20CI-2-green?labelColor=7D898B) ![](https://img.shields.io/badge/Github%20CI-1-blue?labelColor=7D898B)  |
 |Monitoring and Alerting       |![](https://img.shields.io/badge/ELK-4-orange?labelColor=7D898B) ![](https://img.shields.io/badge/Icinga-2-green?labelColor=7D898B) ![](https://img.shields.io/badge/Grafana-3-9400D3?labelColor=7D898B)             |
 |Configuration Management      |![](https://img.shields.io/badge/Ansible-3-9400D3?labelColor=7D898B)                                                                                                                                                 |
+|Infrastructure as Code        |![](https://img.shields.io/badge/Terraform-3-9400D3?labelColor=7D898B)                                                                                                                                               |
 |Web Server                    |![](https://img.shields.io/badge/Nginx-2-green?labelColor=7D898B)                                                                                                                                                    |
-|Scripting Tools               |![](https://img.shields.io/badge/Bash-2-green?labelColor=7D898B) ![](https://img.shields.io/badge/Python-1-blue?labelColor=7D898B)                                                                                   |
+|Scripting Tools               |![](https://img.shields.io/badge/Bash-2-green?labelColor=7D898B) ![](https://img.shields.io/badge/Python-2-green?labelColor=7D898B)                                                                                  |
+|Cloud Services                |![](https://img.shields.io/badge/AWS-2-green?labelColor=7D898B)                                                                                                                                                      |
 
 **Other Tools**
 
 |Types                         |Tools (x/5)                                                                                                                         |
 |------------------------------|------------------------------------------------------------------------------------------------------------------------------------|
-|Web Services                  |![](https://img.shields.io/badge/SOAP-2-green?labelColor=7D898B) ![](https://img.shields.io/badge/REST-2-green?labelColor=7D898B) |
+|Web Services                  |![](https://img.shields.io/badge/SOAP-2-green?labelColor=7D898B) ![](https://img.shields.io/badge/REST-2-green?labelColor=7D898B)   |
 |API Automation                |![](https://img.shields.io/badge/TOSCA%20OSV-5-red?labelColor=7D898B)                                                               |
 |Service Virtualization        |![](https://img.shields.io/badge/TOSCA%20OSV-5-red?labelColor=7D898B)                                                               |
 
